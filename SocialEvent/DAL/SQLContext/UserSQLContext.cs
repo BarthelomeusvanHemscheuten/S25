9225e776b2e1d717148dcb2b8ec7c63f821f6f1d
Thomas <thomasjansen10@hotmail.com>
2017-04-04 11:02:03 +0200

    Methodes aangemaakt in SQL context

--- SocialEvent/DAL/SQLContext/UserSQLContext.cs
+++ SocialEvent/DAL/SQLContext/UserSQLContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,12 +12,6 @@ namespace DAL.SQLContext
     {
         DatabaseConnection databaseConnection = new DatabaseConnection();
         // VALUES, COLUMN NAMEN AANPASSEN AAN DE HAND VAN HUIDIGE DATABASE!! 
-        public string GetPassword(string username)
-        {
-            string query = @"SELECT [Password] FROM [User] WHERE [Username] = '" + username + @"';";
-            return databaseConnection.executeReaderString(query);
-        }
-
         public bool UpdatePassword(string username, string password)
         {
             string query = @"UPDATE [User] SET [Password] = '" + password + @"' WHERE [Username] = '" + username + @"';";
@@ -27,8 +22,6 @@ namespace DAL.SQLContext
         public bool AddUser(string name, string username, string emailAddress, string password, int age)
         { 
             // IK HAD GEEN AUTO INCREMENT BIJ ID, DUS MOEST IK DIT VIA VISUAL STUDIO DOEN. WEGHALEN WANNEER JE WEL AUTO INCREMENT BIJ ID HEBT.
-            string queryGetLastID = @"SELECT COUNT(*) AS [LastID] FROM [User];";
-            int lastID = databaseConnection.executeReaderInt(queryGetLastID) + 1;
             string query = @"INSERT INTO [User]([ID], [Username], [EmailAddress], [Password], [Name], [UserType], [Age]) VALUES (" + lastID + @", '" + username + @"', '" + emailAddress + @"', '" + password + @"', '" + name + @"', " + 2 + @", " + age + @");";
 
             databaseConnection.executeNonQuery(query);
@@ -40,6 +33,52 @@ namespace DAL.SQLContext
             string query = @"DELETE FROM [User] WHERE [Username] = '" + username + @"';";
             databaseConnection.executeNonQuery(query);
             return false;
+        }
+
+        public bool checkLogin(string username, string password)
+        {
+            string query = @"SELECT password FROM user";
+            return false;
+        }
+
+        public int getUserGroup(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool getIsPresent(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime getDateOfBirth(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string getEmail(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string getAdres(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string getTelNr(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string getRFID(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Image getImage(string username)
+        {
+            throw new NotImplementedException();
         }
     }
 }
