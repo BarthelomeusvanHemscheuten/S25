0935eed2664ac9b0aacbda0e8db9ebd9cbcccf35
Thomas <thomasjansen10@hotmail.com>
2017-04-04 10:10:21 +0200

    Progress on user context

--- SocialEvent/DAL/Interfaces/IUser.cs
+++ SocialEvent/DAL/Interfaces/IUser.cs
@@ -8,9 +8,14 @@ namespace DAL.Interfaces
 {
     public interface IUser
     {
-        string GetPassword(string username);
+        bool checkLogin(string username, string password);
         bool UpdatePassword(string username, string password);
         bool AddUser(string name, string username, string emailAddress, string password, int age);
         bool DeleteUser(string username);
+
+        int getUserGroup(string username);
+        bool getIsPresent(string username);
+
+
     }
 }
